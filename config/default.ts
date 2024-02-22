7b3dfb63d2dfe10d1004c9e7f2a75dd1672872bf
Levi <leviszaboo@icloud.com>
2024-02-22 21:44:47 +0100

    adding routes

--- config/default.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-
-export default {
-  port: process.env.PORT || 3000,
-  dbHost: process.env.DB_HOST,
-  dbUser: process.env.DB_USER,
-  dbPassword: process.env.DB_PASSWORD,
-  dbName: process.env.DB_NAME,
-};
